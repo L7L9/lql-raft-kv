90b39c7a59640a16ecfb6dbf5e0af566dc099a75
lql <1964757607@qq.com>
2024-01-09 14:16:50 +0800

    init:初始化项目

---
+++ src/main/java/com/lql/raft/NodeApplication.java
@@ -0,0 +1,15 @@
+package com.lql.raft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lql
+ * @date 2024/01/09
+ */
+@SpringBootApplication
+public class NodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NodeApplication.class,args);
+    }
+}
