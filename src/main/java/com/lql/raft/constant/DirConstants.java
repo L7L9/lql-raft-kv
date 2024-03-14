333f11391dcb5ff94613ccf8301f8ad0ff03a84b
lql <1964757607@qq.com>
2024-03-14 23:13:19 +0800

    feat(constant):添加数据库目录常量类

---
+++ src/main/java/com/lql/raft/constant/DirConstants.java
@@ -0,0 +1,13 @@
+package com.lql.raft.constant;
+
+/**
+ * 数据库目录常量类
+ * @author lql
+ * @date 2024/03/14
+ */
+public class DirConstants {
+    /**
+     * 数据存储根目录
+     */
+    public static String ROOT_DIR = "./raft-database/";
+}
