6f26ca355aa6e7fa984cb4d5b184bf239b93e972
lql <1964757607@qq.com>
2024-03-14 23:13:53 +0800

    feat(service):添加目录服务接口

---
+++ src/main/java/com/lql/raft/service/LogService.java
@@ -0,0 +1,36 @@
+package com.lql.raft.service;
+
+import com.lql.raft.entity.LogEntity;
+
+/**
+ * 日志模块服务接口
+ *
+ * @author lql
+ * @date 2024/03/14
+ */
+public interface LogService {
+    /**
+     * 写入日志实体
+     * @param logEntity 日志实体类
+     */
+    void write(LogEntity logEntity);
+
+    /**
+     * 根据索引获取rocksdb中对应的日志实体信息
+     * @param index 索引
+     * @return 日志实体类
+     */
+    LogEntity get(Long index);
+
+    /**
+     * 获取最后一条日志
+     * @return 日志实体类
+     */
+    LogEntity getLast();
+
+    /**
+     * 获取最后一条日志的索引
+     * @return 索引值，没有返回-1
+     */
+    Long getLastIndex();
+}
