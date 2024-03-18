07835cb7d10b3ef13b9ffeda43c298b772d98b67
lql <1964757607@qq.com>
2024-03-18 14:31:59 +0800

    feat(utils):添加关于时间的工具类

---
+++ src/main/java/com/lql/raft/utils/TimeUtils.java
@@ -0,0 +1,16 @@
+package com.lql.raft.utils;
+
+/**
+ * 时间有关的
+ * @author lql
+ * @date 2024/03/17
+ */
+public class TimeUtils {
+    /**
+     * 获取当前时间的时间戳
+     * @return long 时间戳
+     */
+    public static long currentTime(){
+        return System.currentTimeMillis();
+    }
+}
