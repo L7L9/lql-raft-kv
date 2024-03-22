9196ce1d53381603e7a597bafb73cbdafabf623e
lql <1964757607@qq.com>
2024-03-22 14:43:19 +0800

    feat(rpc):实现追加日志rpc接口

--- src/main/java/com/lql/raft/rpc/ConsistencyService.java
+++ src/main/java/com/lql/raft/rpc/ConsistencyService.java
@@ -4,6 +4,7 @@ import com.lql.raft.config.NodeConfig;
 import com.lql.raft.constant.NodeStatus;
 import com.lql.raft.entity.LogEntity;
 import com.lql.raft.entity.Node;
+import com.lql.raft.entity.Operation;
 import com.lql.raft.rpc.proto.*;
 import com.lql.raft.service.LogService;
 import com.lql.raft.utils.StringUtils;
@@ -12,6 +13,8 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -24,9 +27,6 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ConsistencyService extends ConsistencyServiceGrpc.ConsistencyServiceImplBase {
     @Resource
     private Node node;
-
-    @Resource
-    private NodeConfig nodeConfig;
 
     @Resource
     private LogService logService;
@@ -46,69 +46,106 @@ public class ConsistencyService extends ConsistencyServiceGrpc.ConsistencyServic
     @Override
     public void voteRequest(VoteParam request, StreamObserver<VoteResponse> responseObserver) {
         VoteResponse.Builder response = VoteResponse.newBuilder().setVoteGranted(false);
-        if(voteLock.tryLock()){
-            // 判断当前节点是否任期比他新
-            if(node.getCurrentTerm() > request.getTerm()){
-                responseObserver.onNext(response.build());
-                responseObserver.onCompleted();
-                voteLock.unlock();
-                return;
-            }
-            if(StringUtils.isEmpty(node.getVotedFor()) || node.getVotedFor().equals(request.getCandidateId())){
-                LogEntity logEntity = logService.getLast();
-                if(logEntity != null && logEntity.getTerm() > request.getLastLogTerm()){
-                    responseObserver.onNext(response.build());
-                    responseObserver.onCompleted();
-                    voteLock.unlock();
+        try {
+            if (voteLock.tryLock()) {
+                // 判断当前节点是否任期比他新
+                if (node.getCurrentTerm() > request.getTerm()) {
                     return;
                 }
-                if(logService.getLastIndex() > request.getLastLogIndex()){
-                    responseObserver.onNext(response.build());
-                    responseObserver.onCompleted();
-                    voteLock.unlock();
-                    return;
-                }
-                node.setStatus(NodeStatus.FOLLOW);
-                node.setVotedFor(request.getCandidateId());
-                node.setCurrentTerm(request.getTerm());
+                if (StringUtils.isEmpty(node.getVotedFor()) || node.getVotedFor().equals(request.getCandidateId())) {
+                    LogEntity logEntity = logService.getLast();
+                    if (logEntity != null && logEntity.getTerm() > request.getLastLogTerm()) {
+                        return;
+                    }
+                    if (logService.getLastIndex() > request.getLastLogIndex()) {
+                        return;
+                    }
+                    node.setStatus(NodeStatus.FOLLOW);
+                    node.setVotedFor(request.getCandidateId());
+                    node.setCurrentTerm(request.getTerm());
 
-                response.setVoteGranted(true).setTerm(node.getCurrentTerm());
-                responseObserver.onNext(response.build());
-                responseObserver.onCompleted();
-                voteLock.unlock();
-                return;
+                    response.setVoteGranted(true).setTerm(node.getCurrentTerm());
+                    return;
+                }
             }
+            responseObserver.onNext(response.build());
+        } finally {
+            responseObserver.onNext(response.build());
+            responseObserver.onCompleted();
+            voteLock.unlock();
         }
-        responseObserver.onNext(response.build());
-        responseObserver.onCompleted();
     }
 
     @Override
     public void appendEntriesRequest(AppendEntriesParam request, StreamObserver<AppendEntriesResponse> responseObserver) {
-        AppendEntriesResponse.Builder response = AppendEntriesResponse.newBuilder();
-        response.setSuccess(false);
-        // 获取不到锁返回false
-        if(!appendLock.tryLock()){
+        AppendEntriesResponse.Builder response = AppendEntriesResponse.newBuilder().setSuccess(false);
+        try{
+            // 获取不到锁返回false
+            if(!appendLock.tryLock()){
+                return;
+            }
+
+            response.setTerm(node.getCurrentTerm());
+            // 如果当前节点大于请求节点的任期,返回false
+            if(node.getCurrentTerm() > request.getTerm()){
+                return;
+            } else{
+                // 由于当前节点有可能为其他状态，在自身任期小于等于发送请求的节点任期时，将自身状态转为follower
+                node.setStatus(NodeStatus.FOLLOW);
+            }
+
+            // 重置计时器
+            node.setPreHeartBeatTime(TimeUtils.currentTime());
+            node.setPreElectionTime(TimeUtils.currentTime());
+            // 判断是否为心跳,为心跳则没有携带新日志
+            if(request.getEntriesCount() == 0){
+                // TODO
+                response.setTerm(node.getCurrentTerm());
+                return;
+            }
+            // 判断是否有条目在prevLogIndex上能和prevLogTerm匹配上
+            LogEntity logEntity = logService.get(request.getPreLogIndex());
+            if(Objects.isNull(logEntity) || !logEntity.getTerm().equals(request.getPreLogTerm())){
+                return;
+            }
+
+            List<Log> logList = request.getEntriesList();
+            // 判断是否有条目和新条目产生冲突
+            long nextLogIndex = request.getPreLogIndex() + 1;
+            LogEntity newLog = logService.get(nextLogIndex);
+            if(!Objects.isNull(newLog) && newLog.getTerm() != logList.get(0).getTerm()){
+                // 有冲突,将当前节点后面的条目统统删除
+                long lastIndex = logService.getLastIndex();
+                while(nextLogIndex <= lastIndex){
+                    // 删除后续的条目
+                    logService.delete(nextLogIndex);
+                    ++nextLogIndex;
+                }
+            }
+
+            // 追加未存在的新条目
+            for(Log log:logList){
+                Operation operation = null;
+                if(log.hasOperate()){
+                    operation = new Operation();
+                    operation.setKey(log.getOperate().getKey());
+                    operation.setKey(log.getOperate().getValue());
+                }
+                logService.write(new LogEntity()
+                        .setIndex(log.getIndex())
+                        .setTerm(log.getTerm())
+                        .setOperation(operation));
+            }
+
+            // 设置条目索引
+            if(request.getLeaderCommit() > node.getCommitIndex()){
+                long min = Math.min(request.getLeaderCommit(),logService.getLastIndex());
+                node.setCommitIndex(min);
+            }
+        } finally {
             responseObserver.onNext(response.build());
             responseObserver.onCompleted();
-        }
-
-        response.setTerm(node.getCurrentTerm());
-        // 如果当前节点大于请求节点的任期,返回false
-        if(node.getCurrentTerm() > request.getTerm()){
-            responseObserver.onNext(response.build());
-            responseObserver.onCompleted();
-        }
-
-        node.setPreHeartBeatTime(TimeUtils.currentTime());
-        node.setPreElectionTime(TimeUtils.currentTime());
-        // 判断是否为心跳,为心跳则没有携带新日志
-        if(request.getEntriesCount() == 0){
-
-
-
-
-
+            appendLock.unlock();
         }
     }
 }
