a5d2f1758e5baca5f7a61fc8a4c477f6bd807cbf
lql <1964757607@qq.com>
2024-03-22 14:43:36 +0800

    feat(entity):添加Accessor注解

--- src/main/java/com/lql/raft/entity/LogEntity.java
+++ src/main/java/com/lql/raft/entity/LogEntity.java
@@ -1,12 +1,14 @@
 package com.lql.raft.entity;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * 日志实体类
  * @author lql
  * @date 2024/03/14
  */
+@Accessors(chain = true)
 @Data
 public class LogEntity {
     private Long index;
