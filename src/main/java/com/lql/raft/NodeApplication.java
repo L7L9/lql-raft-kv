40b2bafc4b2215f8021dfdd41fd6e5d164b8480d
lql <1964757607@qq.com>
2024-03-26 12:33:19 +0800

    feat:添加初始化配置以及服务

--- src/main/java/com/lql/raft/NodeApplication.java
+++ src/main/java/com/lql/raft/NodeApplication.java
@@ -1,18 +1,58 @@
 package com.lql.raft;
 
-import com.lql.raft.utils.SpringUtils;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import com.lql.raft.config.NodeConfig;
+import com.lql.raft.entity.Node;
+import com.lql.raft.factory.GrpcServerFactory;
+import com.lql.raft.rpc.ConsistencyService;
+import com.lql.raft.service.impl.LogServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 /**
  * @author lql
  * @date 2024/01/09
  */
-@SpringBootApplication
+@Slf4j
 public class NodeApplication {
-    public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(NodeApplication.class, args);
-        SpringUtils.setContext(applicationContext);
+    public static void main(String[] args) throws InterruptedException {
+        // 1.配置初始化
+        NodeConfig nodeConfig = new NodeConfig();
+        Node node = new Node();
+        try {
+            // 初始化
+            nodeConfig.init();
+
+            // 2.初始化服务
+            LogServiceImpl.getInstance().init(nodeConfig.getPort());
+
+            // 3.初始化node
+            node.setNodeConfig(nodeConfig);
+            // 4.开启grpc服务
+            GrpcServerFactory factory = GrpcServerFactory.getInstance();
+            factory.initAndStart(nodeConfig.getGrpcPort(), new ConsistencyService(node));
+
+            // 5.启动
+            node.init();
+        } catch (UnknownHostException e) {
+            log.error("config init error,reason: {}",e.getMessage());
+        } catch (IOException e) {
+            log.error("grpc server start failed,reason: {}",e.getMessage());
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            synchronized (node) {
+                node.notifyAll();
+            }
+        }));
+
+        log.info("gracefully wait");
+
+        synchronized (node) {
+            node.wait();
+        }
+
+        LogServiceImpl.getInstance().destroy();
     }
 }
