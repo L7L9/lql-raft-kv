0dcb766afdcadf248bd501c16d91c091984e90ea
lql <1964757607@qq.com>
2024-03-28 00:06:49 +0800

    feat(entity):添加replication方法

--- src/main/java/com/lql/raft/entity/Node.java
+++ src/main/java/com/lql/raft/entity/Node.java
@@ -2,15 +2,25 @@ package com.lql.raft.entity;
 
 import com.lql.raft.config.NodeConfig;
 import com.lql.raft.constant.NodeStatus;
+import com.lql.raft.manager.GrpcServerManager;
+import com.lql.raft.rpc.proto.AppendEntriesParam;
+import com.lql.raft.rpc.proto.AppendEntriesResponse;
+import com.lql.raft.rpc.proto.Log;
 import com.lql.raft.service.LogService;
 import com.lql.raft.service.impl.LogServiceImpl;
 import com.lql.raft.thread.ThreadPoolFactory;
 import com.lql.raft.thread.task.ElectoralTask;
 import com.lql.raft.thread.task.HeartBeatTask;
+import com.lql.raft.utils.ConvertUtils;
+import com.lql.raft.utils.TimeUtils;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.Future;
 
 /**
  * node节点：表示当前节点的状态
@@ -87,6 +97,11 @@ public class Node {
      */
     private volatile long preHeartBeatTime = 0L;
 
+    /**
+     * 日志复制的ttl
+     */
+    private final static int REPLICATION_TTL = 20 * 1000;
+
     public void init(){
         // 1.初始化节点状态
         status = NodeStatus.FOLLOW;
@@ -102,5 +117,78 @@ public class Node {
         ThreadPoolFactory.scheduleAtFixedRate(new ElectoralTask(this),6000,500);
 
         log.info("raft-node start success,node-address: {}",nodeConfig.getAddress());
+    }
+
+    /**
+     * 日志复制
+     * @param logEntity 日志
+     * @return 成功返回true
+     */
+    public synchronized Future<Boolean> replication(Peer peer,LogEntity logEntity){
+        return ThreadPoolFactory.submit(()->{
+            long start = TimeUtils.currentTime();
+            long end = start;
+
+            // 一定时间内可以重试
+            while(end - start <= REPLICATION_TTL){
+                AppendEntriesParam.Builder builder = AppendEntriesParam.newBuilder();
+
+                //设置追加日志rpc请求参数
+                builder.setTerm(this.getCurrentTerm());
+                builder.setLeaderId(nodeConfig.getAddress());
+                builder.setLeaderCommit(this.getCommitIndex());
+
+                Long nextIndexTemp = this.nextIndex.get(peer);
+                List<Log> logList = new ArrayList<>();
+                if(logEntity.getIndex() >= nextIndexTemp){
+                    for(long i = nextIndexTemp;i <= logEntity.getIndex();i++){
+                        LogEntity temp = logService.get(i);
+                        if(Objects.nonNull(temp)){
+                            logList.add(ConvertUtils.logEntityToLog(temp));
+                        }
+                    }
+                } else {
+                    // 包装
+                    logList.add(ConvertUtils.logEntityToLog(logEntity));
+                }
+
+                // 获取最小日志
+                LogEntity preLogEntity = logService.get(logList.get(0).getIndex());
+                if(Objects.isNull(preLogEntity)){
+                    builder.setPreLogIndex(0L).setPreLogTerm(0L);
+                } else {
+                    builder.setPreLogIndex(preLogEntity.getIndex()).setPreLogTerm(preLogEntity.getTerm());
+                }
+                AppendEntriesParam param = builder.addAllEntries(logList).build();
+
+                AppendEntriesResponse response = GrpcServerManager.getInstance()
+                        .getConsistencyServiceBlockingStub(peer.getAddr()).appendEntriesRequest(param);
+
+                // 处理结果
+                if (response.getSuccess()){
+                    // 成功，更新matchIndex和nextIndex
+                    this.matchIndex.put(peer, logEntity.getIndex());
+                    this.nextIndex.put(peer, logEntity.getIndex() + 1L);
+                    return true;
+                } else{
+                    // 检测任期
+                    if(response.getTerm() > this.currentTerm){
+                        log.warn("node[{}] term[{}] greater than my term[{}],",peer.getAddr(),response.getTerm(),this.currentTerm);
+                        this.currentTerm = response.getTerm();
+                        this.status = NodeStatus.FOLLOW;
+                        return false;
+                    } else {
+                        if(nextIndexTemp == 0L){
+                            ++nextIndexTemp;
+                        }
+                        nextIndex.put(peer,nextIndexTemp - 1);
+                        log.warn("follower address:{}, nextIndex do not match",peer.getAddr());
+                    }
+                }
+                end = TimeUtils.currentTime();
+            }
+
+            return false;
+        });
     }
 }
