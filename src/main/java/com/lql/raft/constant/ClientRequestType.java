9485894aee91a2750a6c520c8a81b2da013364ee
lql <1964757607@qq.com>
2024-03-30 22:14:50 +0800

    feat(constant):添加Client请求类型常量类

---
+++ src/main/java/com/lql/raft/constant/ClientRequestType.java
@@ -0,0 +1,18 @@
+package com.lql.raft.constant;
+
+/**
+ * 客户端请求类型常量类
+ * @author lql
+ * @date 2024/03/30
+ */
+public class ClientRequestType {
+    /**
+     * 获取请求
+     */
+    public static final int GET = 0;
+
+    /**
+     * 更新请求
+     */
+    public static final int PUT = 1;
+}
